f3c332501166a71048629323ef7edbd7524000ce
unknown <antonhilman103@gmail.com>
2020-12-14 08:01:49 +0700

    [F1][F2][F3] Autentifikasi

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+ 
+use Illuminate\Support\Facades\Route; 
+ 
+Route::get('/', 'AuthController@showFormLogin')->name('login');
+Route::get('login', 'AuthController@showFormLogin')->name('login');
+Route::post('login', 'AuthController@login');
+Route::get('register', 'AuthController@showFormRegister')->name('register');
+Route::post('register', 'AuthController@register');
+ 
+Route::group(['middleware' => 'auth'], function () {
+ 
+    Route::get('home', 'HomeController@index')->name('home');
+    Route::get('logout', 'AuthController@logout')->name('logout');
+ 
 });
