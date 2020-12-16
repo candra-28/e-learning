3a223b6cda7d0b2369041b3de42ca26c9959899f
AhmadSuherman <suhermana274@gmail.com>
2020-12-16 20:10:40 +0700

    [W] Memperbaiki kesalahan pemindahan file

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login E-Learning</title>
+    <link rel="stylesheet" href="{{URL::to('vendor/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{URL::to('vendor/assets/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{URL::to('vendor/assets/css/style.css')}}">
+    <link rel="shortcut icon" href="{{URL::to('vendor/assets/images/logo-atas.png')}}" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo text-center">
+                                <img src="{{URL::to('vendor/assets/images/3.svg')}}">
+                            </div>
+                            <h4>Halo! ayo kita mulai</h4>
+                            <h6 class="font-weight-light">Masuk untuk melanjutkan.</h6>
+                            <form action="{{ route('login') }}" method="post" class="pt-3">
+                                @csrf
+                                @if(session('errors'))
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    Something it's wrong:
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                @endif
+                                @if (Session::has('success'))
+                                <div class="alert alert-success">
+                                    {{ Session::get('success') }}
+                                </div>
+                                @endif
+                                @if (Session::has('error'))
+                                <div class="alert alert-danger">
+                                    {{ Session::get('error') }}
+                                </div>
+                                @endif
+                                <div class="form-group">
+                                    <input type="email" name="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email" value="{{old('email')}}">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                                </div>
+                                <div class="mt-3">
+                                    <button type="submit" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn">MASUK</button>
+                                </div>
+                                <div class="my-2 d-flex justify-content-between align-items-center">
+                                    <div class="form-check">
+                                        <label class="form-check-label text-muted">
+                                            <input type="checkbox" class="form-check-input"> Biarkan saya tetap masuk </label>
+                                    </div>
+                                    <a href="#" class="auth-link text-black">Lupa kata sandi?</a>
+                                </div>
+                            </form>
+                            <div class="text-center mt-4 font-weight-light"> Belum punya akun? <a href="{{ route('register')}}" class="text-primary">Daftar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="vendor/assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="vendor/assets/js/off-canvas.js"></script>
+    <script src="vendor/assets/js/hoverable-collapse.js"></script>
+    <script src="vendor/assets/js/misc.js"></script>
+</body>
+
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Register E-Learning</title>
+
+    <link rel="stylesheet" href="{{URL::to('vendor/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{URL::to('vendor/assets/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{URL::to('vendor/assets/css/style.css')}}">
+    <link rel="shortcut icon" href="{{URL::to('vendor/assets/images/logo-atas.png')}}" />
+
+</head>
+
+<body>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo text-center">
+                                <img src="{{URL::to('vendor/assets/images/3.svg')}}">
+                            </div>
+                            <h4>Baru disini?</h4>
+                            <h6 class="font-weight-light">Mendaftar itu mudah. Hanya perlu beberapa langkah. Pilihlah salah satu Tab dibawah</h6>
+
+                            <div class="card">
+                                <ul class="nav nav-tabs profile-tab" role="tablist">
+                                    <li class="nav-item"> <a class="nav-link show active" data-toggle="tab" href="#profile" role="tab" aria-selected="false">Siswa</a> </li>
+                                    <li class="nav-item"> <a class="nav-link show" data-toggle="tab" href="#settings" role="tab" aria-selected="true">Guru</a> </li>
+                                </ul>
+
+                                <div class="tab-content">
+                                    <div class="tab-pane show active" id="profile" role="tabpanel">
+                                        <form class="forms-sample pt-3">
+
+                                            <div class="form-group">
+                                                <label for="exampleInputUsername1">Nomor Induk Siswa</label>
+                                                <input type="text" class="form-control" placeholder="NIS">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputUsername1">Nama Lengkap <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" placeholder="Nama">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Alamat Email <span class="text-danger">*</span></label>
+                                                <input type="email" class="form-control" placeholder="Email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Kata Sandi <span class="text-danger">*</span></label>
+                                                <input type="password" class="form-control" placeholder="Kata Sandi">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputConfirmPassword1">Ulangi Kata Sandi <span class="text-danger">*</span></label>
+                                                <input type="password" class="form-control" placeholder="Ulangi Sandi">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label>Tahun Masuk <span class="text-danger">*</span></label>
+                                                <select name="" id="" class="form-control">
+                                                    <option value="">2020</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label>Kelas <span class="text-danger">*</span></label>
+                                                <select name="" id="" class="form-control">
+                                                    <option value="">XII</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label class="col-sm-4 col-form-label">Jenis Kelamin <span class="text-danger">*</span></label>
+                                                <div class="col-sm-4">
+                                                    <div class="form-check">
+                                                        <label class="form-check-label">
+                                                            <input type="radio" class="form-check-input" name="gender" value="Laki-laki"> Pria <i class="input-helper"></i></label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <div class="form-check">
+                                                        <label class="form-check-label">
+                                                            <input type="radio" class="form-check-input" name="gender" value="Perempuan"> Wanita <i class="input-helper"></i></label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Tempat Lahir</label>
+                                                <input type="text" class="form-control" placeholder="Kota Lahir">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Tanggal, Bulan, Tahun Lahir</label>
+                                                <input class="form-control" type="date">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Agama</label>
+                                                <select name="religion" class="form-control">
+                                                    <option selected disabled>-- Pilih Agama --</option>
+                                                    <option value="Islam">Islam</option>
+                                                    <option value="Kristen">Kristen</option>
+                                                    <option value="Katolik">Katolik</option>
+                                                    <option value="Hindu">Hindu</option>
+                                                    <option value="Budha">Budha</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Alamat Lengkap</label>
+                                                <input type="text" class="form-control" placeholder="Alamat">
+                                            </div>
+
+
+                                            <button type="submit" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn">DAFTAR</button>
+                                        </form>
+                                    </div>
+
+
+                                    <div class="tab-pane show" id="settings" role="tabpanel">
+                                        <form action="{{ route('login') }}" method="post" class="pt-3">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label for="exampleInputUsername1">Nomor Identitas Pegawai</label>
+                                                <input type="text" class="form-control" placeholder="NIP">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputUsername1">Nama Lengkap <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" placeholder="Nama">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Alamat Email <span class="text-danger">*</span></label>
+                                                <input type="email" class="form-control" placeholder="Email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Kata Sandi <span class="text-danger">*</span></label>
+                                                <input type="password" class="form-control" placeholder="Kata Sandi">
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label class="col-sm-4 col-form-label">Jenis Kelamin <span class="text-danger">*</span></label>
+                                                <div class="col-sm-4">
+                                                    <div class="form-check">
+                                                        <label class="form-check-label">
+                                                            <input type="radio" class="form-check-input" name="gender" value="Laki-laki"> Pria <i class="input-helper"></i></label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <div class="form-check">
+                                                        <label class="form-check-label">
+                                                            <input type="radio" class="form-check-input" name="gender" value="Perempuan"> Wanita <i class="input-helper"></i></label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Tempat Lahir</label>
+                                                <input type="text" class="form-control" placeholder="Kota Lahir">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Tanggal, Bulan, Tahun Lahir</label>
+                                                <input class="form-control" type="date">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name_of_birth">Alamat Lengkap</label>
+                                                <input type="text" class="form-control" placeholder="Alamat">
+                                            </div>
+                                            <div class="mt-3">
+                                                <button type="submit" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn">DAFTAR</button>
+                                            </div>
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="text-center mt-4 font-weight-light"> Anda sudah memiliki akun? <a href="{{ route('login') }}" class="text-primary">Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{URL::to('vendor/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <script src="{{URL::to('vendor/assets/js/off-canvas.js')}}"></script>
+    <script src="{{URL::to('vendor/assets/js/hoverable-collapse.js')}}"></script>
+    <script src="{{URL::to('vendor/assets/js/misc.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
