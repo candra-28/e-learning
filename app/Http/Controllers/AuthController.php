f635e4ddbc51346b3dfd0e9e780d7d93287bb809
AhmadSuherman <suhermana274@gmail.com>
2020-12-16 19:44:24 +0700

    [W] Memindahkan file login dan register ke dalam folder auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,68 +1,66 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
- 
+
 use Illuminate\Support\Facades\Auth;
-use Validator;
-use Hash;
-use Session;
+use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use App\User;
- 
- 
+
+
 class AuthController extends Controller
 {
     public function showFormLogin()
     {
-        if (Auth::check()) { 
+        if (Auth::check()) {
             return redirect()->route('home');
         }
-        return view('login');
+        return view('auth.login');
     }
- 
+
     public function login(Request $request)
     {
         $rules = [
             'email'                 => 'required|email',
             'password'              => 'required|string'
         ];
- 
+
         $messages = [
             'email.required'        => 'Email wajib diisi',
             'email.email'           => 'Email tidak valid',
             'password.required'     => 'Password wajib diisi',
             'password.string'       => 'Password harus berupa string'
         ];
- 
+
         $validator = Validator::make($request->all(), $rules, $messages);
- 
-        if($validator->fails()){
+
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput($request->all);
         }
- 
+
         $data = [
             'email'     => $request->input('email'),
             'password'  => $request->input('password'),
         ];
- 
+
         Auth::attempt($data);
- 
-        if (Auth::check()) { 
+
+        if (Auth::check()) {
             return redirect()->route('home');
- 
-        } else { 
+        } else {
             Session::flash('error', 'Email atau password salah');
             return redirect()->route('login');
         }
- 
     }
- 
+
     public function showFormRegister()
     {
-        return view('register');
+        return view('auth.register');
     }
- 
+
     public function register(Request $request)
     {
         $rules = [
@@ -70,7 +68,7 @@ class AuthController extends Controller
             'email'                 => 'required|email|unique:users,email',
             'password'              => 'required|confirmed'
         ];
- 
+
         $messages = [
             'name.required'         => 'Nama Lengkap wajib diisi',
             'name.min'              => 'Nama lengkap minimal 3 karakter',
@@ -81,13 +79,13 @@ class AuthController extends Controller
             'password.required'     => 'Password wajib diisi',
             'password.confirmed'    => 'Password tidak sama dengan konfirmasi password'
         ];
- 
+
         $validator = Validator::make($request->all(), $rules, $messages);
- 
-        if($validator->fails()){
+
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput($request->all);
         }
- 
+
         $user = new User;
         $user->name = ucwords(strtolower($request->name));
         $user->email = strtolower($request->email);
@@ -95,8 +93,8 @@ class AuthController extends Controller
         $user->email_verified_at = \Carbon\Carbon::now();
         $user->role_id = 3;
         $simpan = $user->save();
- 
-        if($simpan){
+
+        if ($simpan) {
             Session::flash('success', 'Register berhasil! Silahkan login untuk mengakses data');
             return redirect()->route('login');
         } else {
@@ -104,12 +102,10 @@ class AuthController extends Controller
             return redirect()->route('register');
         }
     }
- 
+
     public function logout()
     {
-        Auth::logout(); 
-                return redirect()->route('login');
+        Auth::logout();
+        return redirect()->route('login');
     }
- 
- 
 }
